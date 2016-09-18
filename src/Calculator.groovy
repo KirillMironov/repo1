3a26ee6a3e7d9a27c669f47617451bfe2c1d80ed
Kirill <kirill@localhost>
2016-09-18 19:33:55 +0300

    Calculate sum of all elements of 2d array

--- src/Calculator.groovy
+++ src/Calculator.groovy
@@ -8,6 +8,20 @@ class Calculator {
         double volume = 4 / 3 * Math.PI * Math.pow(radius, 3)
         return volume
     }
+    double calculateCylinderVolume(double radius, double height) {
+        double volume = Math.PI * Math.pow(radius, 2)
+        return volume
+    }
+
+    int calculateSum(int[][] array) {
+        int sum = 0
+        for (int row in 0..array.length - 1) {
+            for (int col in 0..array[0].length - 1) {
+                sum = sum + array[row][col]
+            }
+        }
+        return sum
+    }
 
 
 }
--- test/CalculatorTest.groovy
+++ test/CalculatorTest.groovy
@@ -14,6 +14,32 @@ class CalculatorTest extends GroovyTestCase {
         volume = calculator.calculateSphereVolume(2.0)
         assertEquals(33.51032163829112, volume, DELTA)
     }
+    public void testCalculateSphereVolume() {
+        double volume = calculator.calculateCylinderVolume(1.0, 1.0)
+        assertEquals(3.14159265358979323846, volume, DELTA)
+
+        volume = calculator.calculateCylinderVolume(2.0, 2.0)
+        assertEquals(25.13274122871834590768, volume, DELTA)
+    }
+    public void testCalculateSum() {
+        int[][] a1 = [
+                [0, 1, 3],
+                [7, 2, 5],
+                [5, 3, 9],
+                [2, 6, 8]
+        ]
+
+        int sum = calculator.calculateSum(a1)
+        assert sum == 51
+
+        int[][] a2 = [
+                [0, 1, 3],
+                [5, 3, 9],
+                [2, 6, 8]
+        ]
+        sum = calculator.calculateSum(a2)
+        assert sum == 37
+    }
 
 
 }
