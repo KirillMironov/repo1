de729e94087531a9845c32efd1c2201e4654cd37
Kirill <kirill@localhost>
2017-03-04 20:45:06 +0300

    CountDownLatch

---
+++ src/test/java/concurrent/CountDownLatchTest.java
@@ -0,0 +1,16 @@
+package concurrent;
+
+import org.junit.Test;
+
+/**
+ * Created by kmiro on 04.03.2017.
+ */
+public class CountDownLatchTest {
+    @Test
+    public void test(){
+        //Создать и запустить два потока.Дать имена:Первый - cat. Второй - mouse.
+        //Каждый поток в методе run напечатает свое имя.
+        //использовать объект класса CountDownLatch для того, что бы первый поток напечатал свое имя ГАРАНТИРОВАННО после второго.
+        //Использовать методы  класса CountDownLatch: await и countDown
+    }
+}
