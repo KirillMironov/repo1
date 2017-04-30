d933a17510496232a9546e4b4395cab693dd4072
Kirill <kirill@localhost>
2017-04-30 20:32:15 +0300

    Users table

---
+++ helloweb/src/main/java/helloweb/User.java
@@ -0,0 +1,15 @@
+package helloweb;
+
+/**
+ * Created by kmiro on 30.04.2017.
+ */
+public class User {
+    public long id;
+    public String login;
+    public String password;
+    public String salt;
+    public String firstName;
+    public String lastName;
+    public long dob;
+    public String email;
+}
---
+++ helloweb/src/main/java/helloweb/UserDAO.java
@@ -0,0 +1,8 @@
+package helloweb;
+
+/**
+ * Created by kmiro on 30.04.2017.
+ */
+public class UserDAO {
+
+}
