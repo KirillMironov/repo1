d123e228cdebd0a72a9486935792a19a25a89b07
Kirill <kirill@localhost>
2017-12-16 19:51:53 +0300

    Matrix calculate3x3

--- groovy/src/test/java/math/MatrixTest.java
+++ groovy/src/test/java/math/MatrixTest.java
@@ -17,6 +17,10 @@ public class MatrixTest {
         return a * d - b * c;
     }
 
+    public int calculate2x2(int[][] matrix) {
+        return calculate(matrix, 0, 0);
+    }
+
     public int[][] get2x2(int[][] matrix, int row, int col) {
         int[][] result = new int[2][2];
         int rr = 0;
@@ -36,12 +40,6 @@ public class MatrixTest {
             rr++;
         }
         return result;
-    }
-
-    public int calculate3x3(int[][] matrix) {
-
-
-        return 0;
     }
 
     @Test
@@ -74,7 +72,7 @@ public class MatrixTest {
         assertArrayEquals(new int[][]{
                 {5, 6},
                 {8, 9}
-                }, result);
+        }, result);
 
         result = get2x2(matrix, 1, 1);
         assertArrayEquals(new int[][]{
@@ -87,5 +85,33 @@ public class MatrixTest {
                 {1, 2},
                 {4, 5}
         }, result);
+    }
+
+    public int calculate3x3(int[][] matrix) {
+        int result = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int[][] m = get2x2(matrix, 0, i);
+            int value = matrix[0][i] * calculate2x2(m);
+            if (i % 2 == 1) {
+                result = result - value;
+            } else {
+                result = result + value;
+            }
+        }
+
+        return result;
+    }
+
+    @Test
+    public void testCalculate3x3() {
+        int[][] matrix = {
+                {1, 2, 3,},
+                {4, 5, 6,},
+                {7, 8, 9,}
+        };
+
+        int result = calculate3x3(matrix);
+        assertEquals(0, result);
     }
 }
