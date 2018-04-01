ebfcfabd3b1dc0cb1697abfacd822b971d0c6630
Kirill <kirill@localhost>
2018-04-01 18:59:48 +0300

    Connection pool

--- groovy/src/test/java/database/ConnectionPoolTest.java
+++ groovy/src/test/java/database/ConnectionPoolTest.java
@@ -2,6 +2,9 @@ package database;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by kmiro on 24.03.2018.
  */
@@ -10,14 +13,26 @@ public class ConnectionPoolTest {
 
     @Test
     public void opensConnection() {
+        //Prepare test
 
+        //Method under test
+        Connection connection = pool.open();
 
+        //Verify
+        assertNotNull(connection);
+        assertEquals(3, pool.free.size());
+        assertEquals(1, pool.open.size());
     }
 
     @Test(expected = IllegalStateException.class)
     public void openThrowsException() {
-
-
+        pool.open();
+        pool.open();
+        pool.open();
+        pool.open();
+        assertEquals(0, pool.free.size());
+        assertEquals(4, pool.open.size());
+        pool.open();
     }
 
     @Test
