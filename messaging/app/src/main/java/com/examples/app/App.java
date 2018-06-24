d50069c69d40643d553b38611a4b37db3bcf7530
Kirill <kirill@localhost>
2018-06-24 20:18:36 +0300

    Messaging

--- messaging/app/src/main/java/com/examples/app/App.java
+++ messaging/app/src/main/java/com/examples/app/App.java
@@ -1,5 +1,6 @@
 package com.examples.app;
 
+import com.solacesystems.jcsmp.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +18,48 @@ public class App {
     public void task() {
         double t = readTemperature();
         logger.info("t={}",t);
+        send(t);
+    }
+
+    public void doSend(double temperature) throws Exception {
+        final JCSMPProperties properties = new JCSMPProperties();
+        properties.setProperty(JCSMPProperties.HOST, "192.168.0.53");     // host:port
+        properties.setProperty(JCSMPProperties.USERNAME, "username"); // client-username
+        properties.setProperty(JCSMPProperties.PASSWORD, "password"); // client-password
+        properties.setProperty(JCSMPProperties.VPN_NAME, "default"); // message-vpn
+        final JCSMPSession session =  JCSMPFactory.onlyInstance().createSession(properties);
+
+        session.connect();
+
+        final Topic topic = JCSMPFactory.onlyInstance().createTopic("tutorial/topic");
+
+        XMLMessageProducer prod = session.getMessageProducer(new JCSMPStreamingPublishEventHandler() {
+            @Override
+            public void responseReceived(String messageID) {
+                System.out.println("Producer received response for msg: " + messageID);
+            }
+            @Override
+            public void handleError(String messageID, JCSMPException e, long timestamp) {
+                System.out.printf("Producer received error for msg: %s@%s - %s%n",
+                        messageID,timestamp,e);
+            }
+        });
+
+        TextMessage msg = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
+        final String text = String.format("t=%f", temperature);
+        msg.setText(text);
+        System.out.printf("Connected. About to send message '%s' to topic '%s'...%n",text,topic.getName());
+        prod.send(msg,topic);
+        System.out.println("Message sent. Exiting.");
+        session.closeSession();
+    }
+
+    public void send(double temperature) {
+        try{
+            doSend(temperature);
+        }catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
     }
 
     private class Task implements Runnable {
