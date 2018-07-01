431a7ba1365aebf1815d866cd12faaee9fad2199
Kirill <kirill@localhost>
2018-07-01 20:14:10 +0300

    Messaging modules

--- messaging/app/src/main/java/com/examples/app/App.java
+++ messaging/app/src/main/java/com/examples/app/App.java
@@ -11,13 +11,14 @@ import java.util.concurrent.*;
  */
 public class App {
     Logger logger = LoggerFactory.getLogger(getClass());
+
     public double readTemperature() {
         return 30.0;
     }
 
     public void task() {
         double t = readTemperature();
-        logger.info("t={}",t);
+        logger.info("t={}", t);
         send(t);
     }
 
@@ -27,7 +28,7 @@ public class App {
         properties.setProperty(JCSMPProperties.USERNAME, "username"); // client-username
         properties.setProperty(JCSMPProperties.PASSWORD, "password"); // client-password
         properties.setProperty(JCSMPProperties.VPN_NAME, "default"); // message-vpn
-        final JCSMPSession session =  JCSMPFactory.onlyInstance().createSession(properties);
+        final JCSMPSession session = JCSMPFactory.onlyInstance().createSession(properties);
 
         session.connect();
 
@@ -38,26 +39,27 @@ public class App {
             public void responseReceived(String messageID) {
                 System.out.println("Producer received response for msg: " + messageID);
             }
+
             @Override
             public void handleError(String messageID, JCSMPException e, long timestamp) {
                 System.out.printf("Producer received error for msg: %s@%s - %s%n",
-                        messageID,timestamp,e);
+                        messageID, timestamp, e);
             }
         });
 
         TextMessage msg = JCSMPFactory.onlyInstance().createMessage(TextMessage.class);
         final String text = String.format("t=%f", temperature);
         msg.setText(text);
-        System.out.printf("Connected. About to send message '%s' to topic '%s'...%n",text,topic.getName());
-        prod.send(msg,topic);
+        System.out.printf("Connected. About to send message '%s' to topic '%s'...%n", text, topic.getName());
+        prod.send(msg, topic);
         System.out.println("Message sent. Exiting.");
         session.closeSession();
     }
 
     public void send(double temperature) {
-        try{
+        try {
             doSend(temperature);
-        }catch (Exception e) {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
     }
@@ -71,7 +73,6 @@ public class App {
 
     public void start() {
         ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-        service.scheduleWithFixedDelay(new Task(),1000,5000L, TimeUnit.MILLISECONDS);
-
+        service.scheduleWithFixedDelay(new Task(), 1000, 5000L, TimeUnit.MILLISECONDS);
     }
 }
